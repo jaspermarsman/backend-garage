8cc97e350dd824b1b955d9ddf364c92f4ad20a01
jaspermarsman <jaspermarsman@gmail.com>
2022-01-04 18:43:50 +0100

    added appointment model class

---
+++ src/main/java/nl/marsman/garage/model/Appointment.java
@@ -0,0 +1,36 @@
+package nl.marsman.garage.model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDate;
+
+@MappedSuperclass
+public abstract class Appointment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private LocalDate appointmentDate;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDate getAppointmentDate() {
+        return appointmentDate;
+    }
+
+    public void setAppointmentDate(LocalDate appointmentDate) {
+        this.appointmentDate = appointmentDate;
+    }
+
+
+}
